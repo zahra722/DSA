433a2936502a6c5dfeaaee1108b4f12814d916a0
sayed zahra <sayedzahra2001@gmail.com>
2022-09-25 23:14:54 +0530

    first commit

---
+++ array1.py
@@ -0,0 +1,17 @@
+# arr=input("enter word")
+# result = ""
+# for i in range(len(arr)-1,-1,-1):
+#     result = result+arr[i]
+# print(result)
+
+# using functions
+
+# arr=input("enter")
+
+# result = reversed(arr)
+# print(''.join(result))
+
+# using extended slicing
+
+strng = input("enter")
+print(strng[-1::-1])
